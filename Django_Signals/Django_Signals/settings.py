7e8634b67716b26c1c9a884157e58c5d53ceb08b
gk-svg <gk32239@gmail.com>
2020-11-01 19:15:19 +0530

    readme file

--- Django_Signals/Django_Signals/settings.py
+++ Django_Signals/Django_Signals/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mySignals.apps.MysignalsConfig'
 ]
 
 MIDDLEWARE = [
---
+++ Django_Signals/readme.md
@@ -0,0 +1,2 @@
+**Django Signals**
+https://www.youtube.com/watch?v=w4pnWgEgv8E&list=PLbGui_ZYuhigchy8DTw4pX4duTTpvqlh6&index=90
