631fa5b7f0f9d76cf20151fbb19d6802c7ecc9c0
gk-svg <gk32239@gmail.com>
2020-11-02 07:54:01 +0530

    models Signals for save the models

--- Django_Signals/mySignals/signals.py
+++ Django_Signals/mySignals/signals.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.signals import user_logged_in,user_logged_out,user_login_failed
 from django.contrib.auth.models import User
 from django.dispatch import receiver
+from django.db.models.signals import pre_init,pre_save,pre_delete,post_init,post_save,post_delete
+
 
 @receiver(user_logged_in, sender=User)
 def login_succsess(sender, request, user, **kwargs):
@@ -34,3 +36,31 @@ def login_failed(sender, request, credentials, **kwargs):
     print(f"Kwargs--{kwargs}")
 
 #user_login_failed.connect(login_failed) 
+
+@receiver(pre_save, sender=User)
+def at_beginning_save(sender, instance, **kwargs):
+    print("----------------")
+    print("Pre Save Signal at Beginning")
+    print("Sender--",sender)
+    print("Instance--",instance)
+    print(f"Kwargs--{kwargs}")
+    
+@receiver(post_save, sender=User)
+def at_ending_save(sender, instance, created, **kwargs):
+    if created:
+        print("----------------")
+        print("Post Save Signal at Ending")
+        print("New Record")
+        print("Sender--",sender)
+        print("Instance--",instance)
+        print("created--",created)
+        print(f"Kwargs--{kwargs}")
+    else:
+        print("----------------")
+        print("Post Save Signal at Ending")
+        print("Record Updated")
+        print("Sender--",sender)
+        print("Instance--",instance)
+        print("created--",created)
+        print(f"Kwargs--{kwargs}")
+
