0dbf251b943bc56ab884e2ff78fec73d0eb10ecd
gk-svg <gk32239@gmail.com>
2020-11-02 07:11:29 +0530

    Django-Signals(login-signal,logout-signal,login-fail-signal)

--- Django_Signals/mySignals/signals.py
+++ Django_Signals/mySignals/signals.py
@@ -11,4 +11,26 @@ def login_succsess(sender, request, user, **kwargs):
     print("user--",user)
     print(f"Kwargs--{kwargs}")
 
-#user_logged_in.connect(login_succsess,sender=User)
+#user_logged_in.connect(login_succsess,sender=User) 
+
+@receiver(user_logged_out, sender=User)
+def logout_succsess(sender, request, user, **kwargs):
+    print("----------------")
+    print("LogOut Signal ")
+    print("Sender--",sender)
+    print("Request--",request)
+    print("user--",user)
+    print(f"Kwargs--{kwargs}")
+
+#user_logged_out.connect(logout_succsess,sender=User) 
+
+@receiver(user_login_failed)
+def login_failed(sender, request, credentials, **kwargs):
+    print("----------------")
+    print("Login failed Signal ")
+    print("Sender--",sender)
+    print("Request--",request)
+    print("credentials--",credentials)
+    print(f"Kwargs--{kwargs}")
+
+#user_login_failed.connect(login_failed) 
