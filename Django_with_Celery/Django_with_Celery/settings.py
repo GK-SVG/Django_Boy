1bd68709e637a5fe850804c8b76186fb1b4d65eb
gk-svg <gk32239@gmail.com>
2021-01-11 20:23:06 +0530

    added django-celery-results for storing celery results in db

--- Django_with_Celery/Django_with_Celery/settings.py
+++ Django_with_Celery/Django_with_Celery/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'notifications'
+    'notifications',
+    'django_celery_results'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CELERY_RESULT_BACKEND = 'django-db'
+CELERY_CACHE_BACKEND = 'django-cache'
