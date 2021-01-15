28d1bc0bcc24dde8d51f0a45c4a9d2e8f927f9a1
gk-svg <gk32239@gmail.com>
2021-01-15 08:33:25 +0530

    limitOffset pagination in DRF

--- DRF/DRF11/myapp/pagination.py
+++ DRF/DRF11/myapp/pagination.py
@@ -1,7 +1,13 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination,LimitOffsetPagination
 
 class StudentPagination(PageNumberPagination):
     page_size = 4
     page_query_param = 'p'
     page_size_query_param = 'records'
     max_page_size = 7
+
+class StudentLimitffsetPagination(LimitOffsetPagination):
+    default_limit = 3
+    limit_query_param = 'mylimit'
+    offset_query_param = 'myoffset'
+    max_limit = 7
--- DRF/DRF11/myapp/views.py
+++ DRF/DRF11/myapp/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from .models import Students
 from .serializes import StudentSerializer
 from rest_framework.generics import ListCreateAPIView
-from .pagination import StudentPagination
+from .pagination import StudentPagination,StudentLimitffsetPagination
 # from rest_framework.pagination import PageNumberPagination
 # Create your views here.
 
 class StudentsView(ListCreateAPIView):
     queryset = Students.objects.all()
     serializer_class = StudentSerializer
-    pagination_class = StudentPagination
+    # pagination_class = StudentPagination
+    pagination_class = StudentLimitffsetPagination
