2693ac62b8d06a5692cbe6af1dcfb435077274fd
gk-svg <gk32239@gmail.com>
2021-01-21 20:32:01 +0530

    sms send by twilio and 2factor auth

--- Django_with_twilio/send_sms/models.py
+++ Django_with_twilio/send_sms/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+import os
+from twilio.rest import Client
 # Create your models here.
+class SMS(models.Model):
+    mysms = models.CharField(max_length=200)
+
+    def save(self, *args, **kwargs):
+        account_sid = 'Your Account SID'
+        auth_token = 'Your Auth Token'
+        client = Client(account_sid, auth_token)
+        message = client.messages.create(
+                            body={self.mysms},
+                            from_='Twilio Phone Number',
+                            to   ='Phone Number on which sms send'
+                            )
+        print(message.sid)
+        return super().save(*args, **kwargs)
